bb74ddc0fec4e29e4d8fd1ed26cfd2ba5691672a
blebot <keshav29kapur@gmail.com>
2020-01-21 18:56:02 +0530

    trackbar

--- HOUGH_4.py
+++ HOUGH_4.py
@@ -9,12 +9,12 @@ while (cap.isOpened()):
         # CHANGING BGR TO HSV
         hsv_frame = cv.cvtColor(frame, cv.COLOR_BGR2HLS_FULL)
         # CREATING MASK RANGE
-        mask = cv.inRange(hsv_frame, (22, 36, 52), (82, 255, 255))  # 31,136,59  126,255,255
+        mask = cv.inRange(hsv_frame, (39, 31, 94), (82, 255, 255))  # 31,136,59  126,255,255
         res = cv.bitwise_and(frame, frame, mask=mask)
         kernel = np.ones((3, 3), np.uint8)
         res = cv.dilate(res, kernel, iterations=1)
-        res = cv.erode(res, kernel, iterations=5)
-        res = cv.dilate(res, kernel, iterations=6)
+        res = cv.erode(res, kernel, iterations=2)
+        res = cv.dilate(res, kernel, iterations=3)
         # CONTOUR
         gray = cv.cvtColor(res, cv.COLOR_BGR2GRAY)
         ret, thresh = cv.threshold(gray, 127, 255, 0)
@@ -24,7 +24,7 @@ while (cap.isOpened()):
         res = cv.medianBlur(res, 5)
         for cnt in contours:
             x, y, w, h = cv.boundingRect(cnt)
-            if w > 45 and h > 45:
+            if w > 40 and h > 40:
                 x = x - 60
                 y = y - 60
                 w = w + 150
@@ -34,12 +34,13 @@ while (cap.isOpened()):
                 W = w
                 H = h
                 res = cv.rectangle(res, (x, y), (x + w, y + h), (255, 255, 0), 2)
-                circles = cv.HoughCircles(hough, cv.HOUGH_GRADIENT, 1, 200, param1=30, param2=25, minRadius=20, maxRadius=150)
+                circles = cv.HoughCircles(hough, cv.HOUGH_GRADIENT, 1, 200, param1=30, param2=35, minRadius=15, maxRadius=150)
                 if circles is not None:
                     circles = np.uint16(np.around(circles))
                     for (x, y, r) in circles[0, :]:
                         if x<X+H and y<W+Y and x>X and y>Y:
-                            cv.putText(res, "BALL DETECTED", (x, y), cv.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2)
+                            #cv.putText(res, "BALL DETECTED", (x, y), cv.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2)
+                            cv.putText(res, "BALL DETECTED", (500,20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
                             cv.circle(res, (x, y), r, (0, 255, 0), 3)
                             cv.circle(res, (x, y), 2, (0, 0, 255), 3)
         # output= cv.bitwise_or(res,output,)
