1c0ef71b4a813d0bc3e8d8f7c318dcd9eab312cf
blebot <keshav29kapur@gmail.com>
2020-01-23 18:25:13 +0530

    made for rpi

--- HOUGH_4.py
+++ HOUGH_4.py
@@ -9,7 +9,7 @@ while (cap.isOpened()):
         # CHANGING BGR TO HSV
         hsv_frame = cv.cvtColor(frame, cv.COLOR_BGR2HLS_FULL)
         # CREATING MASK RANGE
-        mask = cv.inRange(hsv_frame, (36, 38, 52), (82, 255, 255))  # 31,136,59  126,255,255
+        mask = cv.inRange(hsv_frame, (36, 40, 62), (62, 255, 255))  # 31,136,59  126,255,255
         res = cv.bitwise_and(frame, frame, mask=mask)
         kernel = np.ones((3, 3), np.uint8)
         res = cv.dilate(res, kernel, iterations=1)
@@ -17,8 +17,8 @@ while (cap.isOpened()):
         res = cv.dilate(res, kernel, iterations=2)
         # CONTOUR
         gray = cv.cvtColor(res, cv.COLOR_BGR2GRAY)
-        ret, thresh = cv.threshold(gray, 127, 255, 0)
-        image, contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        ret, thresh = cv.threshold(gray, 90, 255, 0)
+        contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
         hough = cv.cvtColor(res, cv.COLOR_BGR2GRAY)
         # output = res.copy()
         res = cv.medianBlur(res, 5)
@@ -36,14 +36,14 @@ while (cap.isOpened()):
                 W = w
                 H = h
                 res = cv.rectangle(res, (x, y), (x + w, y + h), (255, 255, 0), 2)
-                circles = cv.HoughCircles(hough, cv.HOUGH_GRADIENT, 1, 200, param1=35, param2=30, minRadius=20, maxRadius=150)
+                circles = cv.HoughCircles(hough, cv.HOUGH_GRADIENT, 1, 200, param1=35, param2=35, minRadius=20, maxRadius=150)
                 if circles is not None:
                     circles = np.uint16(np.around(circles))
                     for (x, y, r) in circles[0, :]:
                         if x<X+H and y<W+Y and x>X and y>Y :
                             cv.putText(res, "BALL DETECTED", (500, 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-                            cv.circle(res, (x, y), r, (0, 255, 0), 3)
-                            cv.circle(res, (x, y), 2, (0, 0, 255), 3)
+                            cv.circle(res, (x, y), r, (0, 255, 0), 5)
+                            cv.circle(res, (x, y), 2, (0, 0, 255), 5)
         # output= cv.bitwise_or(res,output,)
         cv.imshow("output", res)
         # cv.imshow('CIRCLE',output)
