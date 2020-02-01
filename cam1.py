50ec4072aeb9066f3f77aac967e3a1c578322023
Blebot0 <57453738+Blebot0@users.noreply.github.com>
2020-02-01 13:09:44 +0530

    Update cam1.py

--- cam1.py
+++ cam1.py
@@ -6,7 +6,7 @@ cap2 = cv2.VideoCapture(1)
 cap.set(3,400)
 cap.set(4,300)
 
-while(cap.isOpened()|| cap2.isOpened()):
+while(cap.isOpened() or cap2.isOpened()):
     ret, frame = cap.read()
     ret, frame1 = cap2.read()
     if ret == True:
