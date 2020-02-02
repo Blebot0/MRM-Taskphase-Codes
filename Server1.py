9ad36dc1371767375a0f19308732329acdd59d1c
Blebot0 <keshav29kapur@gmailc.om>
2020-02-02 14:25:09 +0530

    Modified server Modified Client

--- Server1.py
+++ Server1.py
@@ -2,19 +2,19 @@ import socket
 import cv2
 import numpy as np
 import pickle
-import time
+import pickle
 
-cap = cv2.VideoCapture(-1)
+cap = cv2.VideoCapture(0)
 
 cap.set(3, 200)
 cap.set(4, 150)
 s = socket.socket()
-s.bind(('', 2020))
+s.bind(('localhost', 2021))
 s.listen(4)
 a, c = s.accept()
 flag = 0
-
-time.sleep(3)
+if cap.isOpened() == True:
+    print("Y")
 while (True):
     ret, frame = cap.read()
     if ret == True:
@@ -22,12 +22,12 @@ while (True):
             ja = pickle.dumps("start")
             a.sendall(ja)
             flag=1
-        frame = cv2.imencode('.jpg', frame)
+        frame = cv2.imencode('.jpeg', frame)
         frame = pickle.dumps(frame)
         a.sendall(frame)
-        time.sleep(0.01)
         aja = pickle.dumps("stop")
         a.sendall(aja)
+
 a.close()
 cap.release()
 cv2.destroyAllWindows()
