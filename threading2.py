6661d061906b32b2a429426086918457fcf06184
Blebot0 <keshav29kapur@gmailc.om>
2020-02-02 18:09:38 +0530

    Added threading of two cameras

---
+++ threading2.py
@@ -0,0 +1,37 @@
+import threading
+import cv2
+import time
+
+def cam1():
+    cap1 = cv2.VideoCapture(2)
+    while (cap1.isOpened()):
+        ret,frame1= cap1.read()
+        if ret== True:
+            cv2.imshow("output",frame1)
+            time.sleep(0.01)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    cap1.release()
+    cv2.destroyAllWindows()
+
+def cam2():
+    cap2 = cv2.VideoCapture(0)
+    while (cap2.isOpened()):
+        ret1,frame2= cap2.read()
+        if ret1== True:
+            cv2.imshow("output1",frame2)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    cap2.release()
+    cv2.destroyAllWindows()
+
+
+
+t1= threading.Thread(target=cam1)
+t2= threading.Thread(target= cam2)
+
+if __name__=="__main__":
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
