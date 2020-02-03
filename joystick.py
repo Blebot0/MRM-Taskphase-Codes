83a3d07cce6a284d84579387166be9f24cb9abb1
Blebot0 <keshav29kapur@gmailc.om>
2020-02-03 19:58:06 +0530

    joystick updated

--- joystick.py
+++ joystick.py
@@ -1,14 +1,20 @@
 import pygame
 
 pygame.init()
-print "Joystics: ", pygame.joystick.get_count()
+print("Joystics: ", pygame.joystick.get_count())
 my_joystick = pygame.joystick.Joystick(0)
 my_joystick.init()
 clock = pygame.time.Clock()
 
-while 1:
+def map(x, in_min, in_max, out_min, out_max):
+    return int((x-in_min) * (out_max-out_min) / (in_max-in_min) + out_min)
+while True:
     for event in pygame.event.get():
-        print my_joystick.get_axis(0),  my_joystick.get_axis(1) 
-        clock.tick(40)
+        x = my_joystick.get_axis(0)
+        y = my_joystick.get_axis(1)
+        x = map(x, -1, 1, -512, 512)
+        y = map(y, -1, 1, -512, 512)
+        print(x, y)
+        clock.tick(100)
 
 pygame.quit ()
